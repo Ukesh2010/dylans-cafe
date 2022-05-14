36d99566d6dfc86e5f7c643687b3b2de671208c5
Sabin Koju <sabinkoju72@gmail.com>
2022-05-14 17:11:38 +0545

    menu item basic layout created, menus added

---
+++ pages/api/menu.js
@@ -0,0 +1,407 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export const jsonData = [
+  {
+    name: "Breakfast Serve till 3 pm",
+    description: null,
+    items: [
+      {
+        name: "Full english Breakfast",
+        price: "9.99",
+        description:
+          "Serve Beans,grilled mushrooms,Fried egg,Bacon ,Sausage and toast.",
+      },
+      {
+        name: "Chilli n Cheese toast plain or with eggs",
+        price: "5.99/6.99",
+        description:
+          "Sourdough bloomer with a special cheese mix . Serve with fried eggs or plain",
+      },
+      {
+        name: "Keema ,egg and Pav",
+        price: "5.99/6.99",
+        description:
+          "Indian spiced chicken mince serve with soft bread roll and eggs.",
+      },
+      {
+        name: "Fruit and yoghurt / Granola",
+        price: "5.99/7.99",
+        description: "Seasonal fresh fruit serve with homemade Granola.",
+      },
+    ],
+  },
+  {
+    name: "Fresh eggs omelette",
+    description: "Served with beans , chips or salad",
+    items: [
+      {
+        name: "Gurkhas omelette",
+        price: "6.75",
+        description: null,
+      },
+      {
+        name: "Sausage tomatoes, onion and cheese",
+        price: "6.50",
+        description: null,
+      },
+      {
+        name: "Ham,mushroom,onion and cheese",
+        price: "6.50",
+        description: null,
+      },
+    ],
+  },
+  {
+    name: "Breakfast side",
+    description: null,
+    items: [
+      {
+        name: "Magic Beans ",
+        price: "2.49",
+        description: null,
+      },
+      {
+        name: "Grilled tomatoes 2 pieces",
+        price: "1.50",
+        description: null,
+      },
+      {
+        name: "Fried eggs(2 eggs) ",
+        price: "1.99",
+        description: null,
+      },
+      {
+        name: "Grilled Mushrooms  ",
+        price: "2.00",
+        description: null,
+      },
+      {
+        name: "Bacon (3 rashers)  ",
+        price: "2.50",
+        description: null,
+      },
+      {
+        name: "Sausage (2 pieces)  ",
+        price: "2.50",
+        description: null,
+      },
+      {
+        name: "Fire grilled Toast with butter and Jam",
+        price: "1.99",
+        description: null,
+      },
+      {
+        name: "Pav (soft, spongy and fluffy Indian bread) ",
+        price: "1.49",
+        description: null,
+      },
+    ],
+  },
+  {
+    name: "Sandwich",
+    description: "Choice of bread white or brown filled with salad and sauce",
+    items: [
+      {
+        name: "BBQ Chicken",
+        price: "3.20",
+        description: null,
+      },
+      {
+        name: "Tuna",
+        price: "3.20",
+        description: null,
+      },
+      {
+        name: "Spicy grilled chicken ",
+        price: "4.75",
+        description: null,
+      },
+      {
+        name: "Chicken BLT ",
+        price: "3.99",
+        description: null,
+      },
+    ],
+  },
+  {
+    name: "Battersea special",
+    description: null,
+    items: [
+      {
+        name: "Fish and chips",
+        price: "7.99",
+        description: null,
+      },
+      {
+        name: "Battered Sausage and chips",
+        price: "7.99",
+        description: null,
+      },
+      {
+        name: "Chips",
+        price: "2.99",
+        description: null,
+      },
+      {
+        name: "Cheesy/Chilli Chips",
+        price: "4.99",
+        description: null,
+      },
+      {
+        name: "Masala Chips (Magic masala)",
+        price: "3.25",
+        description: null,
+      },
+    ],
+  },
+  {
+    name: "Kati rolls",
+    description: null,
+    items: [
+      {
+        name: "Paneer/chicken/Lamb seekh",
+        price: "5.99/5.99/6.49",
+        description:
+          "Kati Rolls come hot and readyto-eat on the go, full of meats and vegetarian options, fresh and savoury spices.",
+      },
+    ],
+  },
+  {
+    name: "Burgers",
+    description: null,
+    items: [
+      {
+        name: "Served with chips or crackers Vegetable petty",
+        price: "4.99",
+        description: null,
+      },
+      {
+        name: "Chicken",
+        price: "5.99",
+        description: null,
+      },
+      {
+        name: "Lamb",
+        price: "6.99",
+        description: null,
+      },
+      {
+        name: "Fish",
+        price: "5.99",
+        description: null,
+      },
+    ],
+  },
+  {
+    name: "Noodle Bar",
+    description: null,
+    items: [
+      {
+        name: "Noodle (veg/Egg/chicken)",
+        price: "5.99/6.50/6.99",
+        description:
+          "Stir-fried noodles cooked in indo-chinese style with vegetables , eggs or chicken",
+      },
+      {
+        name: "Thukpa (veg/chicken/special) ",
+        price: "8.99/9.99/10.99",
+        description:
+          "Tibetan origins hot noodle soup in nepalese style with vegetables, chicken or meat and an intensely flavourful broth.",
+      },
+    ],
+  },
+  {
+    name: "Mo:Mos",
+    description: null,
+    items: [
+      {
+        name: "Paneer/chicken/lamb",
+        price: "5.99/5.99/6.99",
+        description:
+          "Famous nepalese steamed dumplings with some form of filling, be it  paneer,chicken or Meat. Served with house special chutney. (6 pieces Served Steam, + £2 for Chilli or Jhol )",
+      },
+    ],
+  },
+  {
+    name: "Street foods",
+    description: null,
+    items: [
+      {
+        name: "Veg Samosa 2 pieces ",
+        price: "3.50",
+        description:
+          "Pastry filled with boiled potatoes, garden peas, spices, coriander and deep fried.",
+      },
+      {
+        name: "Samosa Chaat ",
+        price: "4.99",
+        description:
+          "Most loved north Indian street food, layered with Chole, Yoghourt, Chutneys, Spices and served hot and savoury",
+      },
+      {
+        name: "Pani puri 6 pieces",
+        price: "4.00",
+        description:
+          "Immensely popular Indian street food of crispy, hollow, fried dough balls (puri) stuffed  with boiled potatoes and boiled chickpeas together with spicy tangy water and a sweet chutney",
+      },
+      {
+        name: "Onion Bhaji 3 pieces",
+        price: "3.50",
+        description:
+          "Finely sliced onion smothered in a simple, flagrantly spiced batter and fried to crispy perfection",
+      },
+      {
+        name: "Chilli chicken / paneer",
+        price: "7.99",
+        description:
+          "Marinated chicken or paneer pieces cooked in indo -chinese style with onions, mixed peppers,chilli and spring greens.",
+      },
+      {
+        name: "Amritsari fish 6 pieces ",
+        price: "5.99",
+        description:
+          "One of the most popular and well known fried fish dishes from Punjab , which marinade with gram flour based batter and fried .",
+      },
+      {
+        name: "Lamb choila",
+        price: "8.99",
+        description:
+          "Nepalese ranks amongst one of all-time favourite Newari cuisines –Choila consists of  grilled meat and a range of himalayan spices.",
+      },
+      {
+        name: "Chicken tikka",
+        price: "8.50",
+        description:
+          "Chicken marinade with chef ’s selection of spices ,yoghurt and herbs . cooked in tandoori oven",
+      },
+      {
+        name: "Grilled salmon 2 pieces ",
+        price: "8.99",
+        description: "Spiced pan grill salmon.",
+      },
+    ],
+  },
+  {
+    name: "Nepali khaja set",
+    description: null,
+    items: [
+      {
+        name: "Chicken",
+        price: "9.99",
+        description:
+          "Dhaba style chicken served with a special bhujia/Chiura mix ,salad and chutney",
+      },
+      {
+        name: "Goat meat",
+        price: "10.99",
+        description: "Home cooked goat meat served with bhujia/ Chiura",
+      },
+      {
+        name: "Chana Bhatura",
+        price: "8.99",
+        description:
+          "One of the most popular Punjabi dish ( Chickpeas curry with puffed bhatura bread) liked almost all over",
+      },
+    ],
+  },
+  {
+    name: "Poppadoms",
+    description: "Serve with mango chutney",
+    items: [
+      {
+        name: "Plain poppadoms",
+        price: "0.80",
+        description: null,
+      },
+      {
+        name: "Prawn Crackers",
+        price: "1.50",
+        description: null,
+      },
+      {
+        name: "Far Far",
+        price: "2.00",
+        description: null,
+      },
+    ],
+  },
+  {
+    name: "Chutney and Dips",
+    description: null,
+    items: [
+      {
+        name: "Momos chutney",
+        price: "0.30",
+        description: null,
+      },
+      {
+        name: "Green chutney",
+        price: "0.30",
+        description: null,
+      },
+      {
+        name: "Tamarind chutney",
+        price: "0.30",
+        description: null,
+      },
+      {
+        name: "Mango chutney",
+        price: "0.30",
+        description: null,
+      },
+      {
+        name: "Chilli sauce",
+        price: "0.30",
+        description: null,
+      },
+      {
+        name: "Raita",
+        price: "3.00",
+        description: null,
+      },
+      {
+        name: "Plain yoghourt",
+        price: "2.50",
+        description: null,
+      },
+    ],
+  },
+  {
+    name: "Chutney and Dips",
+    description: "House special Grill",
+    items: [
+      {
+        name: "Grill Chicken sapeta with salad",
+        price: "5.99",
+        description:
+          "Chicken leg(whole)marinade with high range himalayan secret spices and chargrill in tandoori oven.",
+      },
+      {
+        name: "Lamb chops 3 pieces",
+        price: "8.99",
+        description:
+          "In a Special Marinated Of Lime Juice , Ginger, Garlic And Warmed Spices.chargrill in tandoor.",
+      },
+      {
+        name: "Seekh Kabab ",
+        price: "7.99",
+        description:
+          "Mince meat mixed spice and herbs. Cooked in tandooti oven.",
+      },
+      {
+        name: "Mix grill",
+        price: "13.99",
+        description:
+          "combination of chargrill kebab and starters. (1 chop,seekh kabab 2 ,tikka 2 ,1 chicken leg ,1 salmon)",
+      },
+    ],
+  },
+];
+
+export const getMenus = async () => {
+  return {
+    props: {
+      menuItems: jsonData,
+    },
+  };
+};
--- pages/menu/index.js
+++ pages/menu/index.js
@@ -1,20 +1,47 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { getMenus } from "../api/menu";
 
-export default function Menu() {
+export async function getStaticProps() {
+  return getMenus();
+}
+
+export default function Menu({ menuItems }) {
   return (
-    <div>
+    <div className="page-menu">
       <Head>
-        <title>Dylan's Cafe: Menu</title>
+        <title>Dylan's Cafe: Menus</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="main">
-        
+        <section className="section-container">
+          <div className="container">
+            {menuItems.map((category, index) => (
+              <div key={index} className="menu-category">
+                <div className="category-item">
+                  <h3 className="title">{category.name}</h3>
+                  {category?.description && <span>{category.description}</span>}
+                </div>
+                {category.items.map((item, idx) => (
+                  <div key={idx} className={`menu-item`}>
+                    <div className="left-container">
+                      <h6 className="menu-item-name">{item.name}</h6>
+                      {item?.description && (
+                        <p className="menu-item-description">
+                          {item.description}
+                        </p>
+                      )}
+                    </div>
+                    <div className="right-container">
+                      <span className="menu-item-price">£{item.price}</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </section>
       </main>
-
-      <footer className="footer">
-        
-      </footer>
     </div>
-  )
+  );
 }
